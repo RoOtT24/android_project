5a2948451a3312a9b2170371be28b33eecb7bab2
“RoOtT24” <1190579@student.birzeit.edu>
2023-06-23 00:42:23 +0300

    picture insert

--- app/src/main/java/com/example/myproject/SignupActivity.java
+++ app/src/main/java/com/example/myproject/SignupActivity.java
@@ -1,23 +1,44 @@
 package com.example.myproject;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class SignupActivity extends AppCompatActivity {
 
     private  DataBaseHelper dbHelper ;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         int x =0;
@@ -33,6 +54,34 @@ public class SignupActivity extends AppCompatActivity {
         EditText address = findViewById(R.id.Address);
         EditText password = findViewById(R.id.Password);
         EditText confirmPassword = findViewById(R.id.ConfrimPassword);
+        ImageView profileImageView = (ImageView) findViewById(R.id.profilePic);
+        Boolean []imageChanged = {false};
+
+
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode() == Activity.RESULT_OK ){
+                    Intent data = result.getData();
+                    Uri uri = data.getData();
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                        profileImageView.setImageBitmap(bitmap);
+                        imageChanged[0] = true;
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+        profileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                activityResultLauncher.launch(intent);
+            }
+        });
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,58 +93,106 @@ public class SignupActivity extends AppCompatActivity {
                 String userAddress = address.getText().toString().trim();
                 String userPassword = password.getText().toString().trim();
                 String confirmPass = confirmPassword.getText().toString().trim();
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
                 // Validate required fields
-                boolean isValid = true;
+                boolean []isValid = {true};
+
+
+                if(bitmap != null){
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100,byteArrayOutputStream);
+                    byte [] bytes = byteArrayOutputStream.toByteArray();
+                    final String Base64Image = Base64.encodeToString(bytes, Base64.DEFAULT);
+                    RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                    String url = "";
+
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+//                                    textView.setText("Response is: "+ response);
+                                    if(!response.equals("success")){
+                                     isValid[0] = false;
+                                        Toast.makeText(getApplicationContext(), "Uploading image failed!", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(), "Image was uploaded successfully!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+//                            textView.setText("That didn't work!");
+                            Toast.makeText(getApplicationContext(), error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                            isValid[0] = false;
+                        }
+                    }){
+                        protected Map<String, String> getParams(){
+                            Map<String, String> paramV = new HashMap<>();
+                            paramV.put("image", Base64Image);
+                            return paramV;
+                        }
+                    };
+                    queue.add(stringRequest);
+                }
+                else{
+                    isValid[0] = false;
+                }
+
+
                 if (fName.isEmpty()) {
-                    isValid = false;
+                    isValid[0] = false;
                     firstName.setError("First name is required");
                 } else if (fName.length() < 3 || fName.length() > 20) {
-                    isValid = false;
+                    isValid[0] = false;
                     firstName.setError("First name must be between 3 and 20 characters");
                 }
 
                 if (lName.isEmpty()) {
-                    isValid = false;
+                    isValid[0] = false;
                     lastName.setError("Last name is required");
                 } else if (lName.length() < 3 || lName.length() > 20) {
-                    isValid = false;
+                    isValid[0] = false;
                     lastName.setError("Last name must be between 3 and 20 characters");
                 }
                 if (userEmail.isEmpty()) {
-                    isValid = false;
+                    isValid[0] = false;
                     email.setError("Email is required");
                 }
                 if (userPhone.isEmpty()) {
-                    isValid = false;
+                    isValid[0] = false;
                     phone.setError("Phone is required");
                 }
                 if (userAddress.isEmpty()) {
-                    isValid = false;
+                    isValid[0] = false;
                     address.setError("Address is required");
                 }
                 if (userPassword.isEmpty()) {
-                    isValid = false;
+                    isValid[0] = false;
                     password.setError("Password is required");
                 }
                 if (confirmPass.isEmpty()) {
-                    isValid = false;
+                    isValid[0] = false;
                     confirmPassword.setError("Confirm password is required");
                 }
 
                 // Validate email format
                 if (!userEmail.isEmpty() && !isValidEmail(userEmail)) {
-                    isValid = false;
+                    isValid[0] = false;
                     email.setError("Invalid email format");
                 }
 
                 // Validate password format
                 if (!userPassword.isEmpty() && !isValidPassword(userPassword)) {
-                    isValid = false;
+                    isValid[0] = false;
                     password.setError("Invalid password format, must be 8-15 character and contain at least one number, one lowercase letter, and one uppercase letter. ");
                 }
 
-                if (isValid && userPassword.equals(confirmPass)) {
+                if(!imageChanged[0]){
+                    isValid[0] = false;
+                    Toast.makeText(SignupActivity.this, "Insert A Profile Picture!", Toast.LENGTH_SHORT).show();
+                }
+                if (isValid[0] && userPassword.equals(confirmPass)) {
                     // Create a new Student object
                     Student student = new Student(fName, lName, userEmail, userPhone, userAddress, userPassword);
 
@@ -108,7 +205,8 @@ public class SignupActivity extends AppCompatActivity {
 
                     // Clear the input fields
                     clearInputFields();
-                } else {
+                }
+                if(!isValid[0]) {
                     Toast.makeText(SignupActivity.this, "Please fill in all required fields correctly", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -123,6 +221,7 @@ public class SignupActivity extends AppCompatActivity {
         EditText address = findViewById(R.id.Address);
         EditText password = findViewById(R.id.Password);
         EditText confirmPassword = findViewById(R.id.ConfrimPassword);
+        ImageView iv = findViewById(R.id.profilePic);
 
         firstName.setText("");
         lastName.setText("");
@@ -131,6 +230,7 @@ public class SignupActivity extends AppCompatActivity {
         address.setText("");
         password.setText("");
         confirmPassword.setText("");
+        iv.setImageResource(R.drawable.upload_image);
     }
     private boolean isValidEmail(String email) {
         String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
